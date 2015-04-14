fe12e9c6c13357ec9b8136d2746b21350a2915a8
Gabor Molnar <gabormolnar92@gmail.com>
2015-04-14 18:36:35 +0200

    Create config.php

---
+++ perzisztencia/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    
+    // Az osztályokhoz tartozó táblanevek
+    'PenztarTetel' => 'penztar_tetel',
+    'Penztar' => 'penztar',
+    'SzamlaKifizetes' => 'szamla_kifizetes',
+    'SzamlaTetel' => 'szamla_tetel',
+    'Szamla' => 'szamla',
+    'SzamlaTomb' => 'szamla_tomb',
+    'Felhasznalo' => 'felhasznalo',
+    'Ugyfel' => 'ugyfel',
+];
