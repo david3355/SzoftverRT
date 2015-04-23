4911b4dd0a924f00223e1e2b1835456af7addee2
srt2015 <tibi.srt2015@gmail.com>
2015-04-23 09:09:27 +0200

    ERP_API_SERVER
    Alapserver, ami fogad get/post curl kérést, json array választ ad.
    Ha meglesz a terv, h mi kell az ügyfélkapunak, akk elkészülnek az
    osztályai, metódusai!

---
+++ framework/erp_api_server/erp_api_server.php
@@ -0,0 +1,48 @@
+<?
+	/*ERP API server
+	@params:
+		-$_REQUEST
+			-class: betöltendő osztály teljes neve
+				-method: futtatandó metódus neve
+					-params: az osztály/metódus paraméterei
+	
+	@return: JSON array
+		-success: true|false
+		-data: array, a kért adatok
+		-msg: hibakódok*/
+	
+	
+	try
+	{
+		$params=$_REQUEST;
+		$class=$params['class'];
+		$method=$params['method'];
+		
+		//ellenőrzi, h létezik-e az adott osztály, autoload=true -> ha még nem betöltött, de létezik is true
+		if(class_exists($class)===false)
+		{
+			throw new Exception("INVALID_CLASS");
+		}
+		
+		$class=new $class($params);
+		
+		//ellenőrzi, h létezik-e az adott metódus
+		if(method_exists($class, $method)===false)
+		{
+			throw new Exception("INVALID_METHOD");
+		}
+		
+		//lefuttatja a metódust
+		$result['data']=$class->$method();
+		$result['success']=true;
+		
+	}catch(Exception $e)
+	{
+		$result=array();
+		$result['success']=false;
+		$result['msg']=$e->getMessage();
+	}
+	
+	echo json_encode($result);
+	exit();
+?>
