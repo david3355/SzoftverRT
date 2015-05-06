111e183b0e60d9208dc2e53b3a5aadcf96407c84
Gabor Molnar <gabormolnar92@gmail.com>
2015-05-06 09:11:38 +0200

    API modul és Tibi féle API összemontázsolása
    Még szükség lesz a perzisztencia menedzser select metódusára és jól fog
    működni.

--- framework/modules/ugyfel_api/ugyfel_api.php
+++ framework/modules/ugyfel_api/ugyfel_api.php
@@ -4,6 +4,8 @@
 class Ugyfel_API extends API_Module
 {
 
+    private $api_key = array("2e6766863522c270667cd91952db15f5");
+
     /**
      * return array of string
      * Ebben kell a keretrendszer felé megadni, hogy milyen nevű api hívásokat támogat a modul.
@@ -12,6 +14,7 @@ class Ugyfel_API extends API_Module
     function getSupportedFunctions()
     {
         return [
+            'allUgyfel',
             'getUgyfel',
             'postUgyfel',
             'putUgyfel',
@@ -30,9 +33,27 @@ class Ugyfel_API extends API_Module
     function handleRequest($function, array $params, array $data = null)
     {
 
+        if(!$this->checkAuth($params['key'])){
+            header('Content-Type: text/json', false, 403);
+
+            echo json_encode(['msg' => 'Nincs jogosultsága használni ezt a funkciót!']);
+            return;
+        }
+
         $pm = PersistenceManager::getInstance();
 
         switch ($function) {
+            case 'allUgyfel':
+                $data = ['azon' => '0123QWERTY',
+                    'nev' => 'Nev nev',
+                    'cim' => '1111 Varos Cim 1.',
+                    'telefon' => '21321313',
+                    'email' => 'email@email.com'];
+
+                header('Content-Type: text/json', false, 200);
+
+                echo json_encode($data);
+                break;
             case 'getUgyfel':
                 $data = ['azon' => '0123QWERTY',
                     'nev' => 'Nev nev',
@@ -49,29 +70,39 @@ class Ugyfel_API extends API_Module
 
                 header('Content-Type: text/json', false, 200);
 
-                echo json_encode(['msg'=>'Sikeres ügyfél felvétel!']);
+                echo json_encode(['msg' => 'Sikeres ügyfél felvétel!']);
 
                 break;
             case 'putUgyfel':
 
                 header('Content-Type: text/json', false, 200);
 
-                echo json_encode(['msg'=>'Sikeres ügyfél módosítás!']);
+                echo json_encode(['msg' => 'Sikeres ügyfél módosítás!']);
 
                 break;
             case 'deleteUgyfel':
 
                 header('Content-Type: text/json', false, 200);
 
-                echo json_encode(['msg'=>'Sikeres ügyfél törlés!']);
+                echo json_encode(['msg' => 'Sikeres ügyfél törlés!']);
 
                 break;
             default:
                 header('Content-Type: text/json', false, 404);
 
-                echo json_encode(['msg'=>'A keresett funkció nem található!']);
+                echo json_encode(['msg' => 'A keresett funkció nem található!']);
                 break;
         }
 
+    }
+
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    private function checkAuth($key)
+    {
+        return in_array($key, $this->api_key);
     }
 }
