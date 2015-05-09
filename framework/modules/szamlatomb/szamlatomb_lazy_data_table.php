d04a1fdc19d95f39fe14b42269db5f31d102d927
srt2015 <tibi.srt2015@gmail.com>
2015-05-09 13:27:09 +0200

    szla_tomb_lazy_tb
    getdata, init metódusok, majd nézzetek rá h mit kell még pontosítani

--- framework/modules/szamlatomb/szamlatomb_lazy_data_table.php
+++ framework/modules/szamlatomb/szamlatomb_lazy_data_table.php
@@ -2,13 +2,53 @@
 
 class Szamlatomb_Lazy_Data_Table extends Abstract_Lazy_Data_Table {
 
-    protected function getData(array $post = null)
+    private $pm;
+	
+	protected function getData(array $post = null)
     {
-        // TODO: Implement getData() method.
+        $this->numberOfAllRows = $this->pm->select('Szamlatomb',['count(*) as rn'])->exeSelect()[0]['rn'];
+        
+        $this->pm->select('Szamlatomb');
+        if(!empty($this->selectedSortColumn['column']) && !empty($this->selectedSortColumn['dest'])){
+            $this->pm->orderBy($this->selectedSortColumn['column'],$this->selectedSortColumn['dest']);
+        }
+        if($this->selectedPageNumber == 1 || $this->selectedStep == 1){
+            $this->pm->limit($this->selectedPageNumber-1,$this->selectedStep);
+        } else {
+            $this->pm->limit($this->selectedPageNumber,$this->selectedStep);
+        }
+        
+        return $this->pm->exeSelect();
     }
 
     protected function init()
     {
-        // TODO: Implement init() method.
+        $this->pm = PersistenceManager::getInstance();
+
+        $this->tableName = 'szamlatomb-datatable';
+
+        $this->dataColumns = array(
+            "megnevezes" => array(
+                'sortable' => false
+            ),
+            "szamla_elotag" => array(
+                'sortable' => true
+            ),
+            "szamla_aktual_szam" => array(
+                'sortable' => true
+            ),
+            "lezaras_datum" => array(
+                'sortable' => false
+            )
+        );
+        $this->operationColumns = array(
+            array(
+                'name' => 'edit',
+            ),
+            array(
+                'name' => 'delete',
+            )
+        );
+        $this->steps = array(1, 2, 5);
     }
 }
